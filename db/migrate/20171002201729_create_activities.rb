a9ee6567b4a15cafe405e17244485c027d8d3820
Leo Generali <leogen17@gmail.com>
2017-10-02 16:19:21 -0400

    Create activity model

---
+++ db/migrate/20171002201729_create_activities.rb
@@ -0,0 +1,17 @@
+class CreateActivities < ActiveRecord::Migration[5.1]
+  def change
+    create_table :activities do |t|
+      t.string :title
+      t.text :desc
+      t.time :time
+      t.date :date
+      t.float :warmup
+      t.float :activity
+      t.float :cooldown
+      t.boolean :race
+      t.boolean :workout
+
+      t.timestamps
+    end
+  end
+end
