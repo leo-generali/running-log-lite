2b25353bb86b46d6487124cd12a5f4019530eebc
Leo Generali <leogen17@gmail.com>
2017-10-03 11:42:07 -0400

    Display the total mileage of the the run

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,17 @@
 class Activity < ApplicationRecord
+  
+  def total_mileage
+    miles = 0
+    unless self.warmup.nil?
+      miles += self.warmup
+    end
+    unless self.activity.nil?
+      miles += self.activity
+    end
+    unless self.cooldown.nil?
+      miles += self.cooldown
+    end
+    miles
+  end
+
 end
