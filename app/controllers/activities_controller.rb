33fee9aea21134b7bd5abfc53b7812e42833b9b2
Leo Generali <leogen17@gmail.com>
2017-10-03 10:18:10 -0400

    Show activities and add new ones

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,19 @@
+class ActivitiesController < ApplicationController
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.create(task_params)
+    redirect_to root_path
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:activity).permit(:title, :desc, :time, :date, :warmup, :activity, :cooldown, :race, :workout)
+  end
+end
